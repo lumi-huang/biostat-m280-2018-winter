3414c90d59ef284661a43a348a795b6e81ae2862
Lumi Huang <luminghuang@ucla.edu>
2018-02-26 23:54:43 -0800

    Q1-1 need to finish the renderplot

--- hw3/shiny/app.R
+++ hw3/shiny/app.R
@@ -9,7 +9,7 @@
 
 library(shiny)
 library(tidyverse)
-setwd(.)
+setwd(".")
 LCEP <- readRDS("/home/luminghuang/biostat-m280-2018-winter/hw3/LCEP.rds")
 head(LCEP)
 
@@ -19,28 +19,38 @@ ui <- fluidPage(
    # Application title
    titlePanel("LA City Employee Payroll"),
    
-   # Sidebar with a slider input for number of bins 
+   # Sidebar with a slider input for year and pay types
    sidebarLayout(
       sidebarPanel(
         helpText("Total payroll by LA City"),
         #Visualize the total LA City payroll of each year, with breakdown into 
         #base pay, overtime pay, and other pay
-        selectInput("var",)
+        selectInput("pay",
+                    label = "Choose a pay type",
+                    choices = c("Base Pay", "Overtime Pay", "Other Pay"),
+                    selected = "Base Pay")
       ),
       
       # Show a plot of the generated distribution
       mainPanel(
-         plotOutput("")
+         plotOutput("TPLC")
       )
    )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
 
-   })
+server <- function(input, output) {
+     pay_type <- reactive({
+       switch(LCEP$pay,
+              "Base Pay" = LCEP$`Base Pay`,
+              "Overtime Pay" = LCEP$`Overtime Pay`,
+              "Other Pay" = LCEP$`Other Pay (Payroll Explorer)`)
+     })
+     year <- LCEP$Year
+     
+     output$plot <- renderPlot()
+
+
 }
 
 # Run the application 
